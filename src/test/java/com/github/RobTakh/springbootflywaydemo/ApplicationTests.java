f3f0ab5247e1aa785f960471795c3214ead475c6
Takhnenko <takhnenko.robert@gmail.com>
2021-08-10 00:20:05 +0300

    Added table country into SpringBoot + Flyway project

---
+++ src/test/java/com/github/RobTakh/springbootflywaydemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.RobTakh.springbootflywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
